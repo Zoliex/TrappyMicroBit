2fefa22d8344d48e695369bce85c936643d8e8d0
Zoliex <41476528+Zoliex@users.noreply.github.com>
2023-03-22 14:47:31 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,42 @@
 input.onGesture(Gesture.LogoUp, function () {
-    if (!(isCar)) {
+    if (!(isCar) && actualMode == 0) {
         radio.sendValue("DIR", 0)
         basic.showArrow(ArrowNames.South)
     }
 })
-input.onGesture(Gesture.TiltRight, function () {
+input.onButtonPressed(Button.A, function () {
     if (!(isCar)) {
+        if (actualMode == 0) {
+            radio.sendValue("AUTO", 1)
+            basic.showString("A")
+            actualMode = 1
+        } else {
+            radio.sendValue("AUTO", 0)
+            actualMode = 0
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (!(isCar) && actualMode == 0) {
         radio.sendValue("DIR", 2)
         basic.showArrow(ArrowNames.East)
     }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    if (!(isCar)) {
+    if (!(isCar) && actualMode == 0) {
         radio.sendValue("DIR", 3)
         basic.showArrow(ArrowNames.West)
     }
 })
 input.onGesture(Gesture.LogoDown, function () {
-    if (!(isCar)) {
+    if (!(isCar) && actualMode == 0) {
         radio.sendValue("DIR", 1)
         basic.showArrow(ArrowNames.North)
     }
@@ -38,32 +57,31 @@ radio.onReceivedValue(function (name, value) {
             } else {
                 maqueen.motorStop(maqueen.Motors.All)
             }
-        }
-        if (name == "LED") {
-            if (value == 0) {
-                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-            } else if (value == 1) {
-                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            } else if (value == 2) {
-                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        } else if (name == "AUTO") {
+            if (value == 1) {
+                autoMode = 1
             } else {
-                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+                autoMode = 0
+                basic.pause(50)
+                maqueen.motorStop(maqueen.Motors.All)
             }
         }
     }
 })
 input.onGesture(Gesture.ScreenUp, function () {
-    if (!(isCar)) {
+    if (!(isCar) && actualMode == 0) {
         radio.sendValue("DIR", 4)
         basic.showLeds(`
             . . . . .
-            . . . . .
+            . # . # .
             . . # . .
-            . . . . .
+            . # . # .
             . . . . .
             `)
     }
 })
+let autoMode = 0
+let actualMode = 0
 let isCar = false
 radio.setGroup(1)
 isCar = maqueen.Ultrasonic(PingUnit.Centimeters) != 500
@@ -86,12 +104,28 @@ if (isCar) {
         # . # . #
         `)
     radio.sendValue("DIR", 4)
+    actualMode = 0
     basic.pause(1000)
     basic.showLeds(`
         . . . . .
-        . . . . .
+        . # . # .
         . . # . .
-        . . . . .
+        . # . # .
         . . . . .
         `)
 }
+basic.forever(function () {
+    if (autoMode) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 12) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            while (maqueen.Ultrasonic(PingUnit.Centimeters) < 12) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+            }
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        }
+    }
+})
